188373716568815aa1d45a8c2c6bbc561664a7bb
Zanna Fitch <zannafitch2004@gmail.com>
2023-09-13 11:33:43 -0500

    Test: include state information

--- spec/requests/api/v0/healthcare_spec.rb
+++ spec/requests/api/v0/healthcare_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe "Healthcare", type: :request do
     it "should return healthcare facilities near the user with 211 search API. It will return JSON that has 1. facility name, 2. phone number (if available) 3. short description of services, and 4. address details." do
       keyword = "healthcare"
       location = "denver+CO"
+
       get "/api/v0/healthcare?keyword=#{keyword}&location=#{location}"
+
       response_data = JSON.parse(response.body, symbolize_names: true)
       providers = response_data[:data]
 
       expect(response).to be_successful
-      
+
       providers.each do |provider|
         expect(provider).to have_key(:id)
         expect(provider[:id]).to be_an(String)
