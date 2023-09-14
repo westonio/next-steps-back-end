5f4cae10b9b43573398d622bb89be626934a2731
Kaina Cockett <kainacockett@gmail.com>
2023-09-14 08:48:13 -0600

    revert changes

--- app/controllers/api/v0/search_controller.rb
+++ app/controllers/api/v0/search_controller.rb
@@ -1,13 +1,10 @@
 class Api::V0::SearchController < ApplicationController
   def index
-    # begin
+    begin
       result_objects = TwoOneOneService.search(params[:keyword], params[:location])
-      render json: ErrorSerializer.new(result_objects)
-      # render json: DataSerializer.new(result_objects)
-    # rescue 
-      # require 'pry'; binding.pry
-      # render json: ErrorSerializer.format_errors("Invalid Parameters: #{params[:keyword]}, #{params[:location]}"), status: 400
-      # render json: ActiveRecord::errors
-    # end
+      render json: DataSerializer.new(result_objects)
+    rescue 
+      render json: ErrorSerializer.format_errors("Invalid Parameters"), status: 400
+    end
   end
 end
--- app/services/two_one_one_service.rb
+++ app/services/two_one_one_service.rb
@@ -5,21 +5,15 @@ class TwoOneOneService
   end
 
   def self.get_url(url)
-    conn = Faraday.new(url: "https://api.211.org/search/v1/api/") do |faraday|
-      faraday.headers["Api-Key"] = Rails.application.credentials.my_api_key[:primary_key]
-      faraday.request :url_encoded
-      faraday.adapter Faraday.default_adapter
-    end
     response = conn.get(url)
-    puts response
     JSON.parse(response.body, symbolize_names: true)[:results].map do |data|
       FilteredResults.new(data) 
     end
   end
 
-  # def self.conn
-  #   Faraday.new(url: "https://api.211.org/search/v1/api/") do |faraday|
-  #     faraday.headers["Api-Key"] = Rails.application.credentials.my_api_key[:primary_key]
-  #   end
-  # end
+  def self.conn
+    Faraday.new(url: "https://api.211.org/search/v1/api/") do |faraday|
+      faraday.headers["Api-Key"] = Rails.application.credentials.my_api_key[:primary_key]
+    end
+  end
 end
