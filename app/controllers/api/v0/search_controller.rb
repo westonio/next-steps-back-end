3d0b839416bfcf820a41813394835943d31ea65e
Kaina Cockett <kainacockett@gmail.com>
2023-09-13 18:56:57 -0600

    configure rescue

--- app/controllers/api/v0/search_controller.rb
+++ app/controllers/api/v0/search_controller.rb
@@ -5,8 +5,8 @@ class Api::V0::SearchController < ApplicationController
       render json: DataSerializer.new(result_objects)
     rescue 
       # require 'pry'; binding.pry
-      # render json: ErrorSerializer.format_errors("Invalid Parameters"), status: 400
-      render json: result_objects.errors, status: 400
+      render json: ErrorSerializer.format_errors("Invalid Parameters: #{params[:keyword]}, #{params[:location]}"), status: 400
+      # render json: ActiveRecord::errors
     end
   end
 end
