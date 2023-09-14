65ef777df76624d5c9dc50b3ac65c30b46302207
Kaina Cockett <kainacockett@gmail.com>
2023-09-14 08:43:35 -0600

    alter the service

--- app/services/two_one_one_service.rb
+++ app/services/two_one_one_service.rb
@@ -5,6 +5,11 @@ class TwoOneOneService
   end
 
   def self.get_url(url)
+    conn = Faraday.new(url: "https://api.211.org/search/v1/api/") do |faraday|
+      faraday.headers["Api-Key"] = Rails.application.credentials.my_api_key[:primary_key]
+      faraday.request :url_encoded
+      faraday.adapter Faraday.default_adapter
+    end
     response = conn.get(url)
     puts response
     JSON.parse(response.body, symbolize_names: true)[:results].map do |data|
@@ -12,9 +17,9 @@ class TwoOneOneService
     end
   end
 
-  def self.conn
-    Faraday.new(url: "https://api.211.org/search/v1/api/") do |faraday|
-      faraday.headers["Api-Key"] = Rails.application.credentials.my_api_key[:primary_key]
-    end
-  end
+  # def self.conn
+  #   Faraday.new(url: "https://api.211.org/search/v1/api/") do |faraday|
+  #     faraday.headers["Api-Key"] = Rails.application.credentials.my_api_key[:primary_key]
+  #   end
+  # end
 end
