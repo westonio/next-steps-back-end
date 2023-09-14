60066fe1660ffe3e78fdd8b1eb89f8495b1fc308
Kaina Cockett <kainacockett@gmail.com>
2023-09-13 18:28:56 -0600

    fix: working on Heroku error

--- app/controllers/api/v0/healthcare_controller.rb
+++ app/controllers/api/v0/healthcare_controller.rb
@@ -4,7 +4,9 @@ class Api::V0::HealthcareController < ApplicationController
       result_objects = TwoOneOneService.search(params[:keyword], params[:location])
       render json: DataSerializer.new(result_objects)
     rescue 
-      render json: ErrorSerializer.format_errors("Invalid Parameters"), status: 400
+      require 'pry'; binding.pry
+      # render json: ErrorSerializer.format_errors("Invalid Parameters"), status: 400
+      render json: result_objects.errors, status: 400
     end
   end
 end
