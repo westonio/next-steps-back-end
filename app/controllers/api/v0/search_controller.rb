f3fccd73a00009fe197cf73f443b941888ed1fc2
Kaina Cockett <kainacockett@gmail.com>
2023-09-14 10:49:28 -0600

    Heroku fixed

--- app/controllers/api/v0/search_controller.rb
+++ app/controllers/api/v0/search_controller.rb
@@ -1,10 +1,10 @@
 class Api::V0::SearchController < ApplicationController
   def index
-    # begin
+    begin
       result_objects = TwoOneOneService.search(params[:keyword], params[:location])
       render json: DataSerializer.new(result_objects)
-    # rescue 
-    #   render json: ErrorSerializer.format_errors("Invalid Parameters"), status: 400
-    # end
+    rescue 
+      render json: ErrorSerializer.format_errors("Invalid Parameters"), status: 400
+    end
   end
 end
