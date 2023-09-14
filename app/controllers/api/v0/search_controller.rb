13a423dd233f5d47ef2338cbd9d6d800e2ab9d0a
Kaina Cockett <kainacockett@gmail.com>
2023-09-13 18:42:12 -0600

    remove pry

--- app/controllers/api/v0/search_controller.rb
+++ app/controllers/api/v0/search_controller.rb
@@ -4,7 +4,7 @@ class Api::V0::SearchController < ApplicationController
       result_objects = TwoOneOneService.search(params[:keyword], params[:location])
       render json: DataSerializer.new(result_objects)
     rescue 
-      require 'pry'; binding.pry
+      # require 'pry'; binding.pry
       # render json: ErrorSerializer.format_errors("Invalid Parameters"), status: 400
       render json: result_objects.errors, status: 400
     end
