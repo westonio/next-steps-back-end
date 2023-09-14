d6780d7b6e39854895fffddb8530f2e943dae6af
Kaina Cockett <kainacockett@gmail.com>
2023-09-14 08:34:51 -0600

    remove rescue to track actual error

--- app/controllers/api/v0/search_controller.rb
+++ app/controllers/api/v0/search_controller.rb
@@ -1,12 +1,13 @@
 class Api::V0::SearchController < ApplicationController
   def index
-    begin
+    # begin
       result_objects = TwoOneOneService.search(params[:keyword], params[:location])
-      render json: DataSerializer.new(result_objects)
-    rescue 
+      render json: ErrorSerializer.new(result_objects)
+      # render json: DataSerializer.new(result_objects)
+    # rescue 
       # require 'pry'; binding.pry
-      render json: ErrorSerializer.format_errors("Invalid Parameters: #{params[:keyword]}, #{params[:location]}"), status: 400
+      # render json: ErrorSerializer.format_errors("Invalid Parameters: #{params[:keyword]}, #{params[:location]}"), status: 400
       # render json: ActiveRecord::errors
-    end
+    # end
   end
 end
