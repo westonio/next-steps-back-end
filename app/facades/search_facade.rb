6937e78117a2007a2bb201ea7893a468298b1b1f
Weston Schutt <wtschutt@gmail.com>
2023-09-15 14:24:24 -0600

    Fix: Refactor/update search facade with initialization method

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,12 +1,15 @@
 class SearchFacade
-  
-  def search(keyword, location)
-    
+
+  def initialize(keyword, location)
+    @keyword = keyword
+    @location = location
+  end
+
+  def search
     service = TwoOneOneService.new
-    
-    json = service.search(keyword, location)
-      json[:results].map do |provider_data|
-        FilteredProvider.new(provider_data, keyword)
+    json = service.search(@keyword, @location)
+    json[:results].map do |provider_data|
+      FilteredProvider.new(provider_data, @keyword)
     end
   end
 end
