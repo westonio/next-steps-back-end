341c0eecc5c7c13afd751c6861d95854c6735c16
Weston Schutt <wtschutt@gmail.com>
2023-09-16 09:35:01 -0600

    Test: Add tests for provider details request from FE (happy path)

---
+++ spec/requests/api/v0/provider_details_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'Search', type: :request do
+  describe 'GET /api/v0/provider_details/:id', :vcr do
+    it 'should return the details of a provider with a valid id' do
+      provider_id = "211contrac-1561"
+
+      get "/api/v0/provider_details/#{provider_id}"
+
+      expect(response).to be_successful
+
+      response_data = JSON.parse(response.body, symbolize_names: true)
+      provider = response_data[:data]
+      
+      expect(provider).to have_key(:id)
+      expect(provider[:id]).to be_an(String)
+      
+      expect(provider).to have_key(:type)
+      expect(provider[:type]).to eq("detailed_provider")
+
+      expect(provider[:attributes]).to have_key(:name)
+      expect(provider[:attributes][:name]).to be_a(String)
+
+      expect(provider[:attributes]).to have_key(:description)
+      expect(provider[:attributes][:description]).to be_a(String)
+
+      expect(provider[:attributes]).to have_key(:description)
+      expect(provider[:attributes][:description]).to be_a(String)
+
+      expect(provider[:attributes]).to have_key(:street)
+      expect(provider[:attributes][:street]).to be_a(String)
+
+      expect(provider[:attributes]).to have_key(:city)
+      expect(provider[:attributes][:city]).to be_a(String)
+
+      expect(provider[:attributes]).to have_key(:state)
+      expect(provider[:attributes][:state]).to be_a(String)
+
+      expect(provider[:attributes]).to have_key(:lat)
+      expect(provider[:attributes][:lat]).to be_a(String)
+
+      expect(provider[:attributes]).to have_key(:lon)
+      expect(provider[:attributes][:lon]).to be_a(String)
+    end
+  end
+end
