f166f05eea369592512b6969077d4d783ef4db60
Weston Schutt <wtschutt@gmail.com>
2023-09-16 13:34:22 -0600

    Fix: Add tests to DetailedProvider poro to handle phone number edge casing

--- spec/poros/detailed_providers_spec.rb
+++ spec/poros/detailed_providers_spec.rb
@@ -33,16 +33,6 @@ RSpec.describe DetailedProvider, type: :poros do
                           :document=>"None",
                           :contact=>[],
                           :phone=>[{:idPhone=>"8996837316245339326", :number=>"(800) 244-0680", :extension=>nil, :type=>"main number", :description=>nil, :name=>nil}],
-                          :taxonomy=>
-                            [{:idTaxonomy=>"7880547121544954498",
-                              :code=>"LT-2600.1500",
-                              :term=>"Dialysis Centers",
-                              :nameLevel1=>"Health Care",
-                              :nameLevel2=>"Specialized Treatment and Prevention",
-                              :nameLevel3=>"Hemodialysis",
-                              :nameLevel4=>"Dialysis Centers",
-                              :nameLevel5=>nil,
-                              :nameLevel6=>nil}],
                           :category=>nil,
                           :serviceAtLocation=>[{:idServiceAtLocation=>"211contrac-1561", :idLocation=>"211contrac-1617", :idService=>"211contrac-1561"}],
                           :temporaryMessage=>nil,
@@ -79,15 +69,7 @@ RSpec.describe DetailedProvider, type: :poros do
                             [{:idServiceAtLocation=>"211contrac-1561", :idLocation=>"211contrac-1617", :idService=>"211contrac-1561"},
                             {:idServiceAtLocation=>"211contrac-1562", :idLocation=>"211contrac-1617", :idService=>"211contrac-1562"}]}],
                         :source=>"211 Contra Costa",
-                        :createdOn=>nil,
-                        :lastUpdated=>nil,
-                        :lastVerified=>nil,
-                        :partitionKey=>"LT-2600.1500",
-                        :id=>nil,
-                        :_rid=>nil,
-                        :_self=>nil,
-                        :_etag=>nil,
-                        :_attachments=>nil}
+                      }
 
         provider = DetailedProvider.new(symbolized_data)
 
@@ -105,6 +87,149 @@ RSpec.describe DetailedProvider, type: :poros do
         expect(provider.phone).to eq("(800) 244-0680")
         expect(provider.fees).to eq("Varies, according to health plan.")
         expect(provider.schedule).to eq("Monday-Friday, 5am-6:30pm; Saturday: 7-11am")
+    end
+
+    it 'can get the phone number from the location (if available)' do
+      symbolized_data = {:idOrganization=>"211contrac-213",
+                          :name=>"DAVITA HEALTHCARE PARTNERS",
+                          :alternateName=>nil,
+                          :description=>
+                          "Provides dialysis services for those diagnosed with chronic kidney failure, a condition also known as chronic kidney disease (CKD). Provides locations of over 2,000 outpatient dialysis facilities and acute units in over 800 hospitals 46 states and the District of Columbia. Website offers extensive information about CKD.",
+                          :url=>"http://www.davita.com",
+                          :taxStatus=>nil,
+                          :taxId=>"None",
+                          :yearIncorporated=>nil,
+                          :legalStatus=>"For Profit",
+                          :services=>
+                          [{:idService=>"211contrac-1561",
+                            :idOrganization=>"211contrac-213",
+                            :name=>"DAVITA HEALTHCARE PARTNERS (DIALYSIS CENTERS)",
+                            :alternateName=>nil,
+                            :description=>
+                              "Provides dialysis services for those diagnosed with chronic kidney failure, a condition also known as chronic kidney disease (CKD). Provides locations of over 2,000 outpatient dialysis facilities and acute units in over 800 hospitals 46 states and the District of Columbia. Website offers extensive information about CKD.",
+                            :email=>nil,
+                            :url=>"http://www.davita.com",
+                            :status=>"active",
+                            :applicationProcess=>"Call for information or use website to locate dialysis clinics by city and/or zip code",
+                            :waitTime=>nil,
+                            :fees=>"Varies, according to health plan.",
+                            :accreditations=>nil,
+                            :schedule=>"Monday-Friday, 5am-6:30pm; Saturday: 7-11am",
+                            :language=>"English only",
+                            :eligibility=>"Anyone in need of dialysis",
+                            :document=>"None",
+                            :contact=>[],
+                            :phone=>[{:idPhone=>"8996837316245339326", :number=>"(800) 244-0680", :extension=>nil, :type=>"main number", :description=>nil, :name=>nil}],
+                            :category=>nil,
+                            :serviceAtLocation=>[{:idServiceAtLocation=>"211contrac-1561", :idLocation=>"211contrac-1617", :idService=>"211contrac-1561"}],
+                            :temporaryMessage=>nil,
+                            :createdOn=>nil,
+                            :lastUpdated=>nil,
+                            :lastVerified=>nil}],
+                          :locations=>
+                          [{:idLocation=>"211contrac-1617",
+                            :name=>"DAVITA HEALTHCARE PARTNERS",
+                            :alternateName=>nil,
+                            :description=>
+                              "Provides dialysis services for those diagnosed with chronic kidney failure, a condition also known as chronic kidney disease (CKD). Provides locations of over 2,000 outpatient d
+                        ialysis facilities and acute units in over 800 hospitals 46 states and the District of Columbia. Website offers extensive information about CKD.",
+                            :transportation=>nil,
+                            :latitude=>"39.753627",
+                            :longitude=>"-105.003635",
+                            :schedule=>"Monday-Friday, 5am-6:30pm; Saturday: 7-11am",
+                            :address=>
+                              [{:idAddress=>"2509270918712944620",
+                                :type=>"physical",
+                                :address_1=>"2000 16th Street",
+                                :address_2=>nil,
+                                :city=>"Denver",
+                                :region=>"Denver",
+                                :state_province=>"CO",
+                                :zipcode=>"80202",
+                                :country=>"United States"}],
+                            :accessibility=>nil,
+                            :language=>nil,
+                            :phone=>
+                              [{:idPhone=>"4429686088590907983", :number=>"(800) 424-6589", :extension=>nil, :type=>"Backdoor Number", :description=>nil, :name=>nil},
+                              {:idPhone=>"6719860891525450389", :number=>"(800) 244-0680", :extension=>nil, :type=>"Main Number", :description=>nil, :name=>nil}],
+                            :serviceAtLocation=>
+                              [{:idServiceAtLocation=>"211contrac-1561", :idLocation=>"211contrac-1617", :idService=>"211contrac-1561"},
+                              {:idServiceAtLocation=>"211contrac-1562", :idLocation=>"211contrac-1617", :idService=>"211contrac-1562"}]}],
+                          :source=>"211 Contra Costa",
+                        }
+      provider = DetailedProvider.new(symbolized_data)
+              
+      expect(provider.phone).to eq("(800) 244-0680")
+    end
+
+    it 'returns message if number is not available' do
+      symbolized_data = {:idOrganization=>"211contrac-213",
+                          :name=>"DAVITA HEALTHCARE PARTNERS",
+                          :alternateName=>nil,
+                          :description=>
+                          "Provides dialysis services for those diagnosed with chronic kidney failure, a condition also known as chronic kidney disease (CKD). Provides locations of over 2,000 outpatient dialysis facilities and acute units in over 800 hospitals 46 states and the District of Columbia. Website offers extensive information about CKD.",
+                          :url=>"http://www.davita.com",
+                          :taxStatus=>nil,
+                          :taxId=>"None",
+                          :yearIncorporated=>nil,
+                          :legalStatus=>"For Profit",
+                          :services=>
+                          [{:idService=>"211contrac-1561",
+                            :idOrganization=>"211contrac-213",
+                            :name=>"DAVITA HEALTHCARE PARTNERS (DIALYSIS CENTERS)",
+                            :alternateName=>nil,
+                            :description=>
+                              "Provides dialysis services for those diagnosed with chronic kidney failure, a condition also known as chronic kidney disease (CKD). Provides locations of over 2,000 outpatient dialysis facilities and acute units in over 800 hospitals 46 states and the District of Columbia. Website offers extensive information about CKD.",
+                            :email=>nil,
+                            :url=>"http://www.davita.com",
+                            :status=>"active",
+                            :applicationProcess=>"Call for information or use website to locate dialysis clinics by city and/or zip code",
+                            :waitTime=>nil,
+                            :fees=>"Varies, according to health plan.",
+                            :accreditations=>nil,
+                            :schedule=>"Monday-Friday, 5am-6:30pm; Saturday: 7-11am",
+                            :language=>"English only",
+                            :eligibility=>"Anyone in need of dialysis",
+                            :document=>"None",
+                            :contact=>[],
+                            :category=>nil,
+                            :serviceAtLocation=>[{:idServiceAtLocation=>"211contrac-1561", :idLocation=>"211contrac-1617", :idService=>"211contrac-1561"}],
+                            :temporaryMessage=>nil,
+                            :createdOn=>nil,
+                            :lastUpdated=>nil,
+                            :lastVerified=>nil}],
+                          :locations=>
+                          [{:idLocation=>"211contrac-1617",
+                            :name=>"DAVITA HEALTHCARE PARTNERS",
+                            :alternateName=>nil,
+                            :description=>
+                              "Provides dialysis services for those diagnosed with chronic kidney failure, a condition also known as chronic kidney disease (CKD). Provides locations of over 2,000 outpatient d
+                        ialysis facilities and acute units in over 800 hospitals 46 states and the District of Columbia. Website offers extensive information about CKD.",
+                            :transportation=>nil,
+                            :latitude=>"39.753627",
+                            :longitude=>"-105.003635",
+                            :schedule=>"Monday-Friday, 5am-6:30pm; Saturday: 7-11am",
+                            :address=>
+                              [{:idAddress=>"2509270918712944620",
+                                :type=>"physical",
+                                :address_1=>"2000 16th Street",
+                                :address_2=>nil,
+                                :city=>"Denver",
+                                :region=>"Denver",
+                                :state_province=>"CO",
+                                :zipcode=>"80202",
+                                :country=>"United States"}],
+                            :accessibility=>nil,
+                            :language=>nil,
+                            :serviceAtLocation=>
+                              [{:idServiceAtLocation=>"211contrac-1561", :idLocation=>"211contrac-1617", :idService=>"211contrac-1561"},
+                              {:idServiceAtLocation=>"211contrac-1562", :idLocation=>"211contrac-1617", :idService=>"211contrac-1562"}]}],
+                          :source=>"211 Contra Costa",
+                        }
+                        
+      provider = DetailedProvider.new(symbolized_data)
+
+      expect(provider.phone).to eq("Phone number not available")
     end
   end
 end
