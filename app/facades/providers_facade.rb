d7a592c1c878ddc7cae742c2753ffb506a7288e8
Weston Schutt <wtschutt@gmail.com>
2023-09-16 10:47:53 -0600

    Test: Add vcr fixtures

--- app/facades/providers_facade.rb
+++ app/facades/providers_facade.rb
@@ -6,6 +6,16 @@ class ProvidersFacade
   def provider_details
     service = TwoOneOneService.new
     json = service.get_provider_details(@id)
-    DetailedProvider.new(json.first)
+    create_provider(json)
+  end
+  
+  
+  private
+  def create_provider(json)
+    if json.is_a?(Hash) && json[:status] == 404
+      raise "Could not find a provider with that ID" 
+    else
+      DetailedProvider.new(json.first)
+    end
   end
 end
