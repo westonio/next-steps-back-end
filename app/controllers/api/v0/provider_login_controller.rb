5dbc64658913c2d4e5821ed0c39d4cbeb9d8fa42
Jorja Fleming <aset284@gmail.com>
2023-09-19 11:09:06 -0600

    repushing

--- app/controllers/api/v0/provider_login_controller.rb
+++ app/controllers/api/v0/provider_login_controller.rb
@@ -1,7 +1,6 @@
 class Api::V0::ProviderLoginController < ApplicationController
-  
   def provider_login
-    begin
+      begin
       provider = Provider.find_by(username: params[:username])
       if provider.authenticate(params[:password])
         render json: DetailedProviderSerializer.new(provider), status: 200
@@ -9,7 +8,6 @@ class Api::V0::ProviderLoginController < ApplicationController
         render json: ErrorSerializer.format_errors("Invalid credentials"), status: 401
       end
     rescue StandardError => error
-      require 'pry'; binding.pry
       render json: ErrorSerializer.format_errors(error.message), status: 401
     end
   end
